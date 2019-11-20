bad302416533a523ef00e0709a6c49ea3bd82202
vhal9 <victor.landin@estudante.ufla.br>
2019-11-20 16:48:09 -0300

    extracao das informacoes dos dados estruturados da wikidata e insercao no db

--- notebook/database/database.py
+++ notebook/database/database.py
@@ -7,15 +7,18 @@ class Database:
         self.c = self.connection.cursor()
         self.createTables()
     def createTables(self):
-        self.c.execute('CREATE TABLE IF NOT EXISTS entidade(id text, name text, desc text, pageid text, url text)')
-        self.c.execute('CREATE TABLE IF NOT EXISTS relation(id text, desc text)')
+        self.c.execute('CREATE TABLE IF NOT EXISTS entitie(id text, name text, desc text, pageid text, url text)')
+        self.c.execute('CREATE TABLE IF NOT EXISTS property(id text, desc text)')
         self.connection.commit()
+    '''entrada: dicionario do tipo: {'id':'', 'name':'', 'desc':'', 'pageid':'', 'url':''} '''
     def insertEntitie(self, entitie):
-        insert = "INSERT INTO entitie VALUES('" + entitite[0] + "','" + entitite[1] + '","' + entitite[2]+ "','" + entitite[3] +"','" + entitite[1] +"')"
+        insert = "INSERT INTO entitie VALUES('" + entitie['id'] + "','" + entitie['name'] + "','" + entitie['desc']+ "','" + entitie['pageid'] +"','" + entitie['url'] +"')"
+        print(insert)
         self.c.execute(insert)
         self.connection.commit()
-    def insertRelation(self,relation):
-        insert = "INSERT INTO relation VALUES('" + relation[0] + "','" + relation[1] + "')"
+    def insertRelation(self,proprietys):
+        '''ID, Desc '''
+        insert = "INSERT INTO property VALUES('" + proprietys[0] + "','" + proprietys[1] + "')"
         print(insert)
         self.c.execute(insert)
         self.connection.commit()

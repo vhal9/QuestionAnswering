e8d5cfe5caa3a92331ba33716dc93446260d6a94
vhal9 <victor.landin@estudante.ufla.br>
2019-11-21 19:26:05 -0300

    correcao de erros no banco de dados

--- notebook/database/database.py
+++ notebook/database/database.py
@@ -7,19 +7,34 @@ class Database:
         self.c = self.connection.cursor()
         self.createTables()
     def createTables(self):
-        self.c.execute('CREATE TABLE IF NOT EXISTS entitie(id text, name text, desc text, pageid text, url text)')
-        self.c.execute('CREATE TABLE IF NOT EXISTS property(id text, desc text)')
+        self.c.execute('CREATE TABLE IF NOT EXISTS entitie(id TEXT PRIMARY KEY NOT NULL, name TEXT, desc TEXT, pageid TEXT, url TEXT)')
+        self.c.execute('CREATE TABLE IF NOT EXISTS property(id TEXT PRIMARY KEY NOT NULL, desc TEXT)')
         self.connection.commit()
     '''entrada: dicionario do tipo: {'id':'', 'name':'', 'desc':'', 'pageid':'', 'url':''} '''
     def insertEntitie(self, entitie):
         insert = "INSERT INTO entitie VALUES('" + entitie['id'] + "','" + entitie['name'] + "','" + entitie['desc']+ "','" + entitie['pageid'] +"','" + entitie['url'] +"')"
-        print(insert)
-        self.c.execute(insert)
-        self.connection.commit()
-    def insertRelation(self,proprietys):
+        try:
+            self.c.execute(insert)
+            self.connection.commit()
+            pass
+        except Exception as e:
+            insert = "erro"
+        
+    def insertProperty(self,propertys):
         '''ID, Desc '''
-        insert = "INSERT INTO property VALUES('" + proprietys[0] + "','" + proprietys[1] + "')"
+        insert = "INSERT INTO property VALUES('" + propertys[0] + "','" + propertys[1] + "')"
         print(insert)
         self.c.execute(insert)
         self.connection.commit()
-
+        '''pass
+        except Exception as e:
+            insert = "erro"
+            print(insert)'''
+    def consultarEntitie(self):
+        consulta = "SELECT * FROM entitie "
+        return self.c.execute(consulta)
+    def consultarProperty(self):
+        consulta = "SELECT * FROM property"
+        return self.c.execute(consulta)
+    def finalizarConnection(self):
+        self.connection.close()
